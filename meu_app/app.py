9221738f94fe1d4960e7ab30ea4801de615a10e8
Juliana Hernandez <j_hernandez_faad@yahoo.com.br>
2023-11-13 18:15:18 -0300

    first commit

--- meu_app/app.py
+++ meu_app/app.py
@@ -1,23 +1,32 @@
 from flask import Flask, render_template, request
 
+import math
+
 app = Flask(__name__)
+
+def calcular_tempo_necessario(principal, taxa, aporte_mensal, montante_desejado):
+    taxa_decimal = taxa / 100.0
+    tempo = 0
+    while principal < montante_desejado:
+        principal = (principal + aporte_mensal) * (1 + taxa_decimal)
+        tempo += 1
+    return tempo
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index_tempo_aporte.html')
 
-@app.route('/calcular_juros_simples', methods=['POST'])
-def calcular_juros_simples():
-    try:
+@app.route('/calcular_tempo', methods=['POST'])
+def calcular_tempo():
+    if request.method == 'POST':
         principal = float(request.form['principal'])
-        taxa_juros = float(request.form['taxa_juros']) / 100
-        tempo = int(request.form['tempo'])
-        
-        juros = principal * taxa_juros * tempo
-        montante = principal + juros
+        taxa = float(request.form['taxa'])
+        aporte_mensal = float(request.form['aporte_mensal'])
+        montante_desejado = float(request.form['montante_desejado'])
 
-        return render_template('index.html', resultado=f'O montante é: R${montante:.2f}')
-    except Exception as e:
-        return render_template('index.html', resultado=f'Erro: {str(e)}')
+        tempo_necessario = calcular_tempo_necessario(principal, taxa, aporte_mensal, montante_desejado)
+        
+        return render_template('resultado_tempo_aporte.html', tempo=tempo_necessario)
+
 if __name__ == '__main__':
     app.run(debug=True)
